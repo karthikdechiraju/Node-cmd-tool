9efbd4205b141973c565cfc864e20ade54472984
Karthik Dechiraju <karthik@pyritetechnologies.com>
2018-02-28 15:26:52 +0530

    Add commands at start of the application

--- api/index.js
+++ api/index.js
@@ -3,16 +3,17 @@ const axios = require('axios');
 module.exports.definition = function(text){
 	axios.get('http://api.wordnik.com/v4/word.json/'+ text +'/definitions?api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then(response => {
 		if (response.data.length > 0) {
-			console.log('>> DEFINITIONS OF ' + text.toUpperCase())
+			console.log('=> DEFINITIONS OF ' + text.toUpperCase())
 			for (var i = 0; i < response.data.length; i++) {
 				console.log('> '+response.data[i].text)
 			}
 		}else{
-			console.log('> No definition found')
+			console.log('=> No definition found')
 		}
-		console.log('\t')
+		console.log('')
 	}).catch(error => {
-	    console.log('> No definition found')
+	    console.log('=> No definition found')
+		console.log('')
 	});
 }
 
@@ -25,16 +26,17 @@ module.exports.synonym = function(text){
 			}
 		}
 		if (syn_array.length > 0) {
-			console.log('>> SYNONYMS OF ' + text.toUpperCase())
+			console.log('=> SYNONYMS OF ' + text.toUpperCase())
 			for (item in syn_array) {
 				console.log('> '+syn_array[item])
 			}
 		}else{
-			console.log('> No synonym found')
+			console.log('=> No synonym found')
 		}
-		console.log('\t')
+		console.log('')
 	}).catch(error => {
-		console.log('> No synonym found')
+		console.log('=> No synonym found')
+		console.log('')
 	});
 }
 
@@ -47,16 +49,17 @@ module.exports.antonym = function(text){
 			}
 		}
 		if (ant_array.length > 0) {
-			console.log('>> ANTONYMS OF ' + text.toUpperCase())
+			console.log('=> ANTONYMS OF ' + text.toUpperCase())
 			for (item in ant_array) {
 				console.log('> '+ant_array[item])
 			}
 		}else{
-			console.log('> No antonym found')
+			console.log('=> No antonym found')
 		}
-		console.log('\t')
+		console.log('')
 	}).catch(error => {
-	    console.log('> No antonym found')
+	    console.log('=> No antonym found')
+		console.log('')
 	});
 }
 
@@ -64,14 +67,15 @@ module.exports.antonym = function(text){
 module.exports.example = function(text){
 	axios.get('http://api.wordnik.com:80/v4/word.json/'+ text +'/topExample?useCanonical=false&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then(response => {
 		if (response.data.text) {
-			console.log('>> EXAMPLES of ' + text.toUpperCase())
+			console.log('=> EXAMPLES of ' + text.toUpperCase())
 			console.log('> '+ response.data.text)
 		}else{
-			console.log('> No example found')
+			console.log('=> No example found')
 		}
-		console.log('\t')
+		console.log('')
 	}).catch(error => {
-	    console.log('> No example found');
+	    console.log('=> No example found');
+		console.log('')
 	});
 }
 
@@ -82,27 +86,28 @@ module.exports.word_of_the_day = function(){
 	var day = date.getDate();
 	axios.get('http://api.wordnik.com:80/v4/words.json/wordOfTheDay?date='+ year+'-'+ month+ '-' + day+ '-&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5').then(response => {
 		if (response.data) {
-			console.log('>> WORD OF THE DAY : '+ response.data.word.toUpperCase())
-			console.log('\t')
+			console.log('=> WORD OF THE DAY : '+ response.data.word.toUpperCase())
+			console.log('')
 			
-			console.log('>> Definitions')
+			console.log('=> Definitions')
 			for (i in response.data.definitions) {
 				console.log('> ' + response.data.definitions[i].text)
 			}
-			console.log('\t')
-			console.log('>> Examples')
+			console.log('')
+			console.log('=> Examples')
 			for (i in response.data.examples) {
 				console.log('> ' + response.data.examples[i].text)
 			}
-			console.log('\t')
-			console.log('>> Note')
+			console.log('')
+			console.log('=> Note')
 			console.log('> ' + response.data.note)
-			console.log('\t')
+			console.log('')
 		}else{
-			console.log('> No word found')
+			console.log('=> No word found')
 		}
-		console.log('\t')
+		console.log('')
 	}).catch(error => {
-	    console.log('> No word found');
+	    console.log('=> No word found');
+		console.log('')
 	});
 }
--- server.js
+++ server.js
@@ -2,18 +2,28 @@ const http = require('http')
 const port = 3000
 var Api = require('./api')
 const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+	console.log(request.url)
+	response.end('Hello Node.js Server!')
 }
 
 const server = http.createServer(requestHandler)
 
 server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
-  }
-
-  console.log(`server is listening on ${port}`)
+	if (err) {
+		return console.log('something bad happened', err)
+	}else{
+		console.log('')
+		console.log('\t\t\tWELCOME TO NODE DICTIONARY\n')
+		console.log('Enter these commands to explore')
+		console.log('def \t => \t Get definitions of a word')
+		console.log('syn \t => \t Get synonyms of a word')
+		console.log('ant \t => \t Get antonyms of a word')
+		console.log('ex \t => \t Get examples of a word')
+		console.log('dict \t => \t Get full details of a word')
+		console.log('today \t => \t Get word of the day (press ENTER to get word of the day)')
+		console.log('clear \t => \t Clear screen\n')
+		console.log('')
+	}
 })
 
 
@@ -23,7 +33,7 @@ stdin.addListener("data", function(d) {
 	var entered_string = d.toString().trim();
 	var string_arr = entered_string.split(' ')
 	if (string_arr[0]) {
-		switch(string_arr[0].trim()){
+		switch(string_arr[0].toLowerCase().trim()){
 			case 'def':
 				Api.definition(string_arr[1])
 				break;
@@ -50,7 +60,10 @@ stdin.addListener("data", function(d) {
 				Api.antonym(string_arr[1])
 				Api.example(string_arr[1])
 				break;
-			
+
+			case 'clear':
+				console.log('\033c')
+				break;
 			default:
 				console.log(string_arr[0] + ' is not defined')
 				break;
